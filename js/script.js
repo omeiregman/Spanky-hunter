aed491efa1e4592ac2f3ce3bffc439e7dc687ef6
= <=>
2017-10-12 01:21:33 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelectorAll('.score');
+const moles = document.querySelectorAll('.mole');
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+function randomTIme(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes){
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  if(hole === lastHole){
+    return randomHole(holes);
+  }
+
+  lastHole = hole;
+  return hole;
+}
+
+function peep() {
+  const time = randomTIme(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add('up');
+  setTimeout(() => {
+    hole.classList.remove('up');
+
+    if (!timeUp) peep();
+  }, time);
+}
+
+function startGame() {
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  peep();
+  setTimeout(() => timeUp = true, 5000)
+}
+
+function shoot(e) {
+  if(!e.isTrusted) return; //fake clicks
+  score++;
+  this.classList.remove('up');
+  scoreBoard.textContent = score;
+  console.log(score);
+}
+
+moles.forEach(mole => mole.addEventListener('click', shoot));
